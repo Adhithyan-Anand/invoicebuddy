017105f14c3c2b324b29f2c6d0a4fc6605e791c0
Adhithyan Anand <adhithyananand@Adhithyans-MacBook-Air.local>
2025-10-14 12:38:54 +0530

    server entrypoint correctly configured for production

--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
-import { setupVite, serveStatic, log } from "./vite";
+import { serveStatic, log } from "./vite";
 import { setupGoogleAuth } from "./googleAuth";
 import session from "express-session";
 import passport from "passport";
@@ -77,11 +77,8 @@ app.use((req, res, next) => {
   // importantly only setup vite in development and after
   // setting up all the other routes so the catch-all route
   // doesn't interfere with the other routes
-  if (app.get("env") === "development") {
-    await setupVite(app, server);
-  } else {
-    serveStatic(app);
-  }
+  // Only serve static files in production, do not import or use Vite
+  serveStatic(app);
 
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
